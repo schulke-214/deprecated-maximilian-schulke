aada40d8c14435468a3e95afc62858a7fce0df84
schulke-214 <schulkemaximilian@gmail.com>
2018-09-14 10:14:24 +0200

    trying out stackoverflow codesnippet

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,24 @@ class Application extends App {
 
     componentDidMount() {
         document.querySelector('body').classList.remove('loading');
+
+        (function preventPullToRefresh(element) {
+            var prevent = false;
+
+            document.querySelector(element).addEventListener('touchstart', function(e){
+            if (e.touches.length !== 1) { return; }
+
+            var scrollY = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+            prevent = (scrollY === 0);
+            });
+
+            document.querySelector(element).addEventListener('touchmove', function(e){
+            if (prevent) {
+                prevent = false;
+                e.preventDefault();
+            }
+            });
+        })("body");
     }
 
     removePageLoader() {
@@ -103,6 +121,8 @@ class Application extends App {
                             </Layout>
                         )}
                     </DeviceContext.Consumer>
+
+                    
                 </Container>
             </DeviceProvider>
         )
