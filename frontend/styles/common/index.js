4ebe8f4e8869c7f49cbdf998ffa0502a683b6df4
schulke-214 <schulkemaximilian@gmail.com>
2018-09-14 10:07:22 +0200

    changed css to prevent pull-to-refresh

--- frontend/styles/common/index.js
+++ frontend/styles/common/index.js
@@ -24,8 +24,13 @@ export default css`
     }
 
     body {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        
         height: 100vh;
         width: 100vw;
+        
         overflow: hidden;
     }
 
