041fee4999139442ba72fbdecfe0cbd3abbd7d64
schulke-214 <schulkemaximilian@gmail.com>
2019-02-17 18:33:23 +0100

    feat: created a basic document and a index page

---
+++ pages/_document.jsx
@@ -0,0 +1,16 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class AppDocument extends Document {
+    render() {
+        return (
+            <html>
+                <Head />
+                <body>
+                    <h1>from Document</h1>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        )
+    }
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,3 @@
+import '../frontend/styles/default.scss';
+
 export default () => <h1>ola</h1>
