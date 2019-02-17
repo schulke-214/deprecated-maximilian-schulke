6738213be3c98930e8d519fca34f286fdc7924ec
schulke-214 <schulkemaximilian@gmail.com>
2019-02-17 18:32:56 +0100

    feat: added global sass defaults

--- next.config.js
+++ next.config.js
@@ -9,17 +9,21 @@ module.exports = withSass({
         importLoaders: 1,
         localIdentName: "[local]___[hash:base64:5]",
     },
-    sassLoaderOptions: {
-        data: `
-            @import ''
-        `
-    },
-
     webpack(config) {
-        config.resolve.alias['components'] = path.join(__dirname, 'frontend/components');
-        config.resolve.alias['styles'] = path.join(__dirname, 'frontend/styles');
-        config.resolve.alias['utility'] = path.join(__dirname, 'frontend/utility');
+        config.resolve.alias['@'] = path.join(process.cwd(), 'components');
+        config.resolve.alias['components'] = path.join(process.cwd(), 'frontend/components');
+        config.resolve.alias['styles'] = path.join(process.cwd(), 'frontend/styles');
+        config.resolve.alias['utility'] = path.join(process.cwd(), 'frontend/utility');
 
+        config.module.rules.push({
+            test: /\.(scss|sass)$/,
+            use: [{
+                loader: 'sass-resources-loader',
+                options: {
+                    resources: path.join(process.cwd(), 'frontend/styles/default.scss')
+                },
+            }, ],
+        })
 
         return config;
     }
