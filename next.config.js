dc840ce8c1000b05a960d0a2de81f85b22efa54a
schulke-214 <schulkemaximilian@gmail.com>
2019-02-18 23:06:19 +0100

    fix: changed the webpack aliases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 
-
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
@@ -10,17 +9,14 @@ module.exports = withSass({
         localIdentName: "[local]___[hash:base64:5]",
     },
     webpack(config) {
-        config.resolve.alias['@'] = path.join(process.cwd(), 'components');
-        config.resolve.alias['components'] = path.join(process.cwd(), 'frontend/components');
-        config.resolve.alias['styles'] = path.join(process.cwd(), 'frontend/styles');
-        config.resolve.alias['utility'] = path.join(process.cwd(), 'frontend/utility');
+        config.resolve.alias['@'] = path.join(process.cwd(), '/app');
 
         config.module.rules.push({
             test: /\.(scss|sass)$/,
             use: [{
                 loader: 'sass-resources-loader',
                 options: {
-                    resources: path.join(process.cwd(), 'frontend/styles/default.scss')
+                    resources: path.join(process.cwd(), '/app/ressources/style-system/default.scss')
                 },
             }, ],
         })
