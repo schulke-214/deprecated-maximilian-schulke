5dad56b2e631fc48df04dcc227500ebc79fd75a0
schulke-214 <schulkemaximilian@gmail.com>
2019-02-19 23:27:11 +0100

    feat: added document again

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,16 +1,56 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, NextScript } from 'next/document';
 
-export default class AppDocument extends Document {
-  render() {
-    return (
-      <html>
-        <Head />
-        <body>
-          <h1>from Document</h1>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+class AppDocument extends Document {
+	render() {
+		return (
+			<html lang='en'>
+				<Head>
+					<meta charSet='UTF-8' />
+					<meta
+						name='viewport'
+						content='initial-scale=1.0, width=device-width'
+						key='viewport'
+					/>
+					<meta
+						name='description'
+						content='Im a creative developer based in bielefeld, germany!' /* REMINDER FOR FUTURE ME: CHANGE THIS*/
+					/>
+					<meta name='theme-color' content='#060606' />
+
+					<link
+						rel='shortcut icon'
+						type='image/png'
+						href='/static/icons/logo-dark-48.png'
+					/>
+					<link rel='manifest' href='/static/manifest.json' />
+					<link rel='stylesheet' href='/static/fonts/fonts.css' />
+
+					<script />
+
+					<noscript>
+						<meta
+							httpEquiv='Refresh'
+							content='0; URL=https://domain.com/lightweight/' /* REMINDER FOR FUTURE ME: CHANGE THIS*/
+						/>
+					</noscript>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		);
+	}
 }
+
+export default AppDocument;
+/**
+ * 
+ * 					{/* 
+					<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenLite.min.js' />
+					<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TimelineLite.min.js' />
+					<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/plugins/CSSPlugin.min.js' />
+					<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/easing/EasePack.min.js' />
+					<script src='https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.7.1/pixi.min.js' /> }
+
+ */
