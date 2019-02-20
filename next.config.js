2cbf23d60cc3bf7dd3cb7e4f84b1bad007ed8dd6
schulke-214 <schulkemaximilian@gmail.com>
2019-02-20 23:20:47 +0100

    fix: added another alias to the store folder

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ module.exports = withSass({
     },
     webpack(config) {
         config.resolve.alias['@'] = path.join(process.cwd(), '/app');
+        config.resolve.alias['store'] = path.join(process.cwd(), '/store');
+
 
         config.module.rules.push({
             test: /\.(scss|sass)$/,
