7356faf4d47b4dabd64bd8c340a386be580cc9e9
schulke-214 <schulkemaximilian@gmail.com>
2019-02-21 23:25:33 +0100

    feat(redux): created a demo store

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,28 @@
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 
-import Layout from '@/components/container/layout';
-import { capitalize } from '@/utility/strings';
+import LocalStorageService from 'services/local-storage';
+import initStore from 'store';
+
+import Layout from 'app/components/container/layout';
+import { generateTitle } from 'app/utility/strings';
+
 // import PageLoader from "components/common/pageLoader/pageLoader";
 
-import initializeStore from 'store/store';
-import LocalStorageService from 'store/local-storage-service';
+const store = initStore();
 
-const store = initializeStore();
+store.subscribe(() => {
+	const { location } = store.getState();
+	LocalStorageService.setState({ location });
+});
 
-// store.subscribe(() => {
-// const { test } = store.getState();
-
-// LocalStorageService.setState({ test });
-// });
+import { device_size_change } from 'store/actions';
+store.dispatch(
+	device_size_change({
+		width: 1020,
+		height: 912
+	})
+);
 
 class Application extends App {
 	// state = {
@@ -31,47 +39,14 @@ class Application extends App {
 	// 	});
 	// };
 
-	dynamicPageTitle = () => {
-		let url = this.props.router.pathname;
-		url = url.slice(1, url.length);
-		url = url.split('/', 2);
-
-		if (url.length === 2) {
-			let projectName = url[1];
-			projectName = projectName.split('-', 3);
-
-			for (let i = 0; i < projectName.length; i++)
-				projectName[i] = capitalize(projectName[i]);
-
-			url[1] = projectName.join(' ');
-		}
-
-		if (url[0] === '') url.shift();
-
-		let pageTitle;
-		switch (url.length) {
-			case 1:
-				pageTitle = capitalize(url[0]);
-				break;
-
-			case 2:
-				pageTitle = capitalize(url[0]) + ' - ' + url[1];
-				break;
-
-			default:
-				pageTitle = 'Maximilian Schulke - Creative Developer';
-				break;
-		}
-
-		return <title>{pageTitle}</title>;
-	};
-
 	render() {
 		const { Component, pageProps } = this.props;
 
 		return (
 			<Container>
-				<Head>{this.dynamicPageTitle()}</Head>
+				<Head>
+					<title>{generateTitle(this.props.router.pathname)}</title>
+				</Head>
 
 				{/* {this.state.pageLoader.isOpen ? (
 					<PageLoader finished={this.removePageLoader} />
