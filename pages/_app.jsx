adf38873460fd7e6d333fe3484e890e112458325
schulke-214 <schulkemaximilian@gmail.com>
2019-02-23 22:26:03 +0100

    feat(redux): added the provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,13 @@
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 
-import LocalStorageService from 'services/local-storage';
 import initStore from 'store';
+import LocalStorageService from 'services/local-storage';
+import { Provider } from 'react-redux';
 
 import Layout from 'app/components/container/layout';
+import DeviceController from 'app/components/container/device-controller';
+
 import { generateTitle } from 'app/utility/strings';
 
 // import PageLoader from "components/common/pageLoader/pageLoader";
@@ -16,13 +19,13 @@ store.subscribe(() => {
 	LocalStorageService.setState({ location });
 });
 
-import { device_size_change } from 'store/actions';
-store.dispatch(
-	device_size_change({
-		width: 1020,
-		height: 912
-	})
-);
+// import { device_size_change } from 'store/actions';
+// store.dispatch(
+// 	device_size_change({
+// 		width: 1020,
+// 		height: 912
+// 	})
+// );
 
 class Application extends App {
 	// state = {
@@ -43,18 +46,20 @@ class Application extends App {
 		const { Component, pageProps } = this.props;
 
 		return (
-			<Container>
-				<Head>
-					<title>{generateTitle(this.props.router.pathname)}</title>
-				</Head>
-				{/* {this.state.pageLoader.isOpen ? (
-					<PageLoader finished={this.removePageLoader} />
-				) : null} */}
-
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</Container>
+			<Provider store={store}>
+				<Container>
+					<Head>
+						<title>{generateTitle(this.props.router.pathname)}</title>
+					</Head>
+					{/* {this.state.pageLoader.isOpen ? (
+                        <PageLoader finished={this.removePageLoader} />
+                    ) : null} */}
+					<DeviceController />
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</Container>
+			</Provider>
 		);
 	}
 }
@@ -93,5 +98,11 @@ render() {
 }
 
 */
+
+const mapDispatchToProps = dispatch => ({
+	setDeviceSize(width, height) {
+		dispatch();
+	}
+});
 
 export default Application;
