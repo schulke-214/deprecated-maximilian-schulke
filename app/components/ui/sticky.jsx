94b9526891f4ecdf495a77b9c61d391af9e0bdd1
schulke-214 <schulkemaximilian@gmail.com>
2019-03-02 17:41:06 +0100

    feat(sticky): modified the ratio and animation dur

--- app/components/ui/sticky.jsx
+++ app/components/ui/sticky.jsx
@@ -14,7 +14,7 @@ class Sticky extends PureComponent {
 	};
 
 	size = this.props.size || 100;
-	stickyRatio = 0.5;
+	stickyRatio = 0.25;
 	entered = false;
 	duration = 0;
 
@@ -80,7 +80,7 @@ class Sticky extends PureComponent {
 	handleMouseEnter = ev => {
 		this.setRect();
 		this.setMouse(ev);
-		this.duration = 2;
+		this.duration = 1.5;
 		this.entered = true;
 
 		if (!this.props.device.isPhone && !this.props.device.isMobile) {
