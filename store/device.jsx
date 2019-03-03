a44434d05b5fab60cbf3377ff381b5caea7e126e
schulke-214 <schulkemaximilian@gmail.com>
2019-03-03 20:45:47 +0100

    feat(store/device): added a new store schema

--- store/device.jsx
+++ store/device.jsx
@@ -1,22 +1,50 @@
 import { cloneDeep } from 'lodash';
 
-const SET_SIZE = 'app/device/SET_SIZE';
-// const SET_TYPE = 'app/device/SET_TYPE';
+const SET_META = 'app/device/SET_META';
+const SET_SCREEN = 'app/device/SET_SCREEN';
+const SET_BROWSER = 'app/device/SET_BROWSER';
+const SET_ENGINE = 'app/device/SET_ENGINE';
+const SET_OS = 'app/device/SET_OS';
 
 const initialState = {
-	size: {
-		width: 0,
-		height: 0,
+	meta: {
+		supported: true,
+		model: 'MacBook',
+		vendor: 'Apple',
+		category: 'desktop',
 		phone: false,
 		tablet: false,
-		desktop: false
+		desktop: true,
+		touch: false
 	},
-	type: {
-		supported: true,
-
-		browser: {
-			engine: null,
-			name: null
+	screen: {
+		width: 0,
+		height: 0
+	},
+	browser: {
+		name: 'Chrome',
+		version: '72.0',
+		major: '72',
+		type: {
+			chrome: true,
+			firefox: false,
+			opera: false,
+			safari: false,
+			mobileSafari: false
+		}
+	},
+	engine: {
+		name: 'WebKit',
+		version: '537.36'
+	},
+	os: {
+		name: 'Mac OS',
+		version: '10.14',
+		type: {
+			mac: true,
+			linux: false,
+			ubuntu: false,
+			windows: false
 		}
 	}
 };
@@ -25,27 +53,70 @@ export default (state = initialState, { type, data }) => {
 	const updated = cloneDeep(state);
 
 	switch (type) {
-		case SET_SIZE:
-			updated.size = {
+		case SET_META:
+			updated.meta = {
+				...updated.meta,
 				...data
 			};
 
 			return updated;
+		case SET_SCREEN:
+			updated.screen = {
+				...updated.screen,
+				...data
+			};
+
+			return updated;
+		case SET_BROWSER:
+			updated.browser = {
+				...updated.browser,
+				...data
+			};
+
+			return updated;
+
+		case SET_ENGINE:
+			updated.engine = {
+				...updated.engine,
+				...data
+			};
+
+			return updated;
+
+		case SET_OS:
+			updated.os = {
+				...updated.os,
+				...data
+			};
+
+			return updated;
+
 		default:
 			return state;
 	}
 };
 
-export const setDeviceSize = data => {
-	return {
-		type: SET_SIZE,
-		data
-	};
-};
+export const setMeta = data => ({
+	type: SET_META,
+	data
+});
 
-// export const setDeviceType = data => {
-// 	return {
-// 		type: SET_TYPE,
-// 		data
-// 	};
-// };
+export const setScreen = data => ({
+	type: SET_SCREEN,
+	data
+});
+
+export const setBrowser = data => ({
+	type: SET_BROWSER,
+	data
+});
+
+export const setEngine = data => ({
+	type: SET_ENGINE,
+	data
+});
+
+export const setOs = data => ({
+	type: SET_OS,
+	data
+});
