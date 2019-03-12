a4eecc9126eb4bbb7c2624e9fa0c7c2afdfa2a64
schulke-214 <schulkemaximilian@gmail.com>
2019-03-12 23:14:21 +0100

    fix: updated imports

--- app/components/data/device-controller.jsx
+++ app/components/data/device-controller.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setMeta, setScreen, setBrowser, setEngine, setOs } from 'store/device';
 
-import DeviceService from 'services/device';
+import DeviceService from 'services/device-detection';
 
 class DeviceController extends React.PureComponent {
 	componentDidMount() {
