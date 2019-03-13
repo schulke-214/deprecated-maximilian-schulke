3e2aa1797b6b0e29e4d5af59f070296f6d414d78
schulke-214 <schulkemaximilian@gmail.com>
2019-03-13 23:29:24 +0100

    feat(PageTransition): added a transition component

---
+++ app/components/ui/page-transition.jsx
@@ -0,0 +1,13 @@
+import { TimelineLite } from 'gsap';
+
+import styles from 'app/scss/styles/ui/page-transition.scss';
+
+const PageTransition = () => {
+	return (
+		<div id='__transition' className={`${styles.container}`}>
+			<span>transition</span>
+		</div>
+	);
+};
+
+export default PageTransition;
