3e02ba1b1edb3f30e3cd2b208c2878b17217f5f2
schulke-214 <schulkemaximilian@gmail.com>
2019-04-13 23:14:12 +0200

    fix: cleaned up the 404 page

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -6,20 +6,6 @@ export default class ErrorPage extends React.Component {
 		return (
 			<>
 				<div>404</div>
-				<div>
-					<h1>Page not found</h1>
-					<iframe
-						src='https://giphy.com/embed/yoJC2Olx0ekMy2nX7W'
-						width='480'
-						height='264'
-						frameBorder='0'
-						className='giphy-embed'
-						allowFullScreen
-					/>
-					<Link href='/'>
-						<a>Go home</a>
-					</Link>
-				</div>
 			</>
 		);
 	}
