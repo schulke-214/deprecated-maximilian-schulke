972868b1ee5c9f671bbf5d1adcb09b59a27b4d67
schulke-214 <schulkemaximilian@gmail.com>
2019-05-05 19:57:28 +0200

    feat: removed console from cookie notice

--- components/layout/legal-notice/legal-notice.jsx
+++ components/layout/legal-notice/legal-notice.jsx
@@ -4,37 +4,15 @@ import LocalStorageService from 'services/local-storage';
 
 import './legal-notice.scss';
 
-const CMD_HELP = () => (
-	<>
-		<span>this is help</span>
-		<span>this is help</span>
-	</>
-);
-
-const CMD_NOT_FOUND = name => {
-	<span>The cmd {name} was not found.</span>;
-};
-
 class LegalNotice extends React.Component {
 	state = {
-		accepted: true,
-		active: false,
-		cmd: '',
-		history: []
+		accepted: true
 	};
-
-	input = React.createRef();
 
 	componentDidMount() {
 		if (LocalStorageService.get('legal-notice') !== true) {
 			this.setState({ accepted: false });
 		}
-
-		addEventListener('click', this.deactivate);
-	}
-
-	componentWillUnmount() {
-		removeEventListener('click', this.deactivate);
 	}
 
 	exec = cmd => {
@@ -52,17 +30,6 @@ class LegalNotice extends React.Component {
 		}
 	};
 
-	activate = ev => {
-		ev.stopPropagation();
-		this.input.current.focus();
-		this.setState({ active: true });
-	};
-
-	deactivate = () => {
-		this.input.current.blur();
-		this.setState({ active: false });
-	};
-
 	agree = () => {
 		LocalStorageService.set('legal-notice', true);
 		this.setState({ accepted: true });
@@ -70,12 +37,8 @@ class LegalNotice extends React.Component {
 
 	decline = () => {
 		this.setState({ accepted: false });
-		localStorage.clear();
+		LocalStorageService.clear();
 		window.location.href = 'https://lightweight.maximilianschulke.com/';
-	};
-
-	handleChange = ev => {
-		this.setState({ cmd: ev.target.value });
 	};
 
 	render() {
@@ -85,54 +48,32 @@ class LegalNotice extends React.Component {
 
 		return (
 			<div className='legal-notice' onClick={this.activate}>
-				<div className='legal-notice__content'>
-					<p>
-						Cookies and IP addresses allow this page to improve your experience. This
-						website uses cookies and collects your IP address for these purposes.
-					</p>
-					<br />
-					<p>
-						Maximilian Schulke may use cookies and my IP address to collect individual
-						statistics. For more information visit the{' '}
-						<Link href='/dataprivacy'>
-							<a className='link--secondary'>dataprivacy</a>
-						</Link>{' '}
-						page or{' '}
-						<a className='link--secondary' href='mailto:legal@maximilianschulke.com'>
-							write an email
-						</a>
-						.
-					</p>
-					<br />
-					<div className='legal-notice__choice-links'>
-						<a onClick={this.agree}>
-							[<span>Y</span>]es, I agree.
-						</a>
-						<a onClick={this.decline}>
-							[<span>N</span>]o, thanks
-						</a>
-					</div>
+				<p>
+					Cookies and IP addresses allow this page to improve your experience. This
+					website uses cookies and collects your IP address for these purposes.
+				</p>
+				<br />
+				<p>
+					Maximilian Schulke may use cookies and my IP address to collect individual
+					statistics. For more information visit the{' '}
+					<Link href='/dataprivacy'>
+						<a className='link--secondary'>dataprivacy</a>
+					</Link>{' '}
+					page or{' '}
+					<a className='link--secondary' href='mailto:legal@maximilianschulke.com'>
+						write an email
+					</a>
+					.
+				</p>
+				<br />
+				<div className='legal-notice__choice-links'>
+					<a onClick={this.agree}>
+						[<span>Y</span>]es, I agree.
+					</a>
+					<a onClick={this.decline}>
+						[<span>N</span>]o, thanks
+					</a>
 				</div>
-				<div className='legal-notice__console'>
-					<span>
-						~ root$
-						<span className='legal-notice__cmd'>{this.state.cmd}</span>
-						<span
-							className={`legal-notice__cursor ${
-								this.state.active ? 'legal-notice__cursor--active' : ''
-							}`}
-							style={{
-								left: 60 + this.state.cmd.length * 7.5 + 'px'
-							}}
-						/>
-					</span>
-				</div>
-				<input
-					className='legal-notice__input'
-					onChange={this.handleChange}
-					maxLength='10'
-					ref={this.input}
-				/>
 			</div>
 		);
 	}
