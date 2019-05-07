95ebc4f47b4482b6d471b59b9949d5acd6d444aa
schulke-214 <schulkemaximilian@gmail.com>
2019-05-07 22:53:32 +0200

    refactor: renamed all components into camelcase

---
+++ components/PageTransition/PageTransition.jsx
@@ -0,0 +1,13 @@
+// import { TimelineLite } from 'gsap';
+
+import './PageTransition.scss';
+
+const PageTransition = () => {
+	return (
+		<div id='__transition'>
+			<span>transition</span>
+		</div>
+	);
+};
+
+export default PageTransition;
