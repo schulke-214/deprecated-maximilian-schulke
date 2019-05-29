7e363ad072249b85854cc5036d3bab732da55a7e
schulke-214 <schulkemaximilian@gmail.com>
2019-05-29 23:59:25 +0200

    feat: add page transition

--- components/page-transition/PageTransition.jsx
+++ components/page-transition/PageTransition.jsx
@@ -1,13 +1,32 @@
-// import { TimelineLite } from 'gsap';
-
 import './PageTransition.scss';
 
-const PageTransition = () => {
-	return (
-		<div id='__transition'>
-			<span>transition</span>
-		</div>
-	);
-};
+class PageTransition extends React.Component {
+	container = React.createRef();
+
+	componentDidMount() {
+		const dur = 1.75;
+
+		this.tl = new TimelineLite({ paused: true });
+
+		this.tl
+			.to(this.container.current, dur, {
+				transformOrigin: 'left',
+				scaleX: 1,
+				ease: Power4.easeInOut
+			})
+			.to(this.container.current, dur, {
+				transformOrigin: 'right',
+				scaleX: 0,
+				delay: 0.5,
+				ease: Power4.easeInOut
+			});
+
+		this.tl.play();
+	}
+
+	render() {
+		return <div className='__transition' ref={this.container} />;
+	}
+}
 
 export default PageTransition;
